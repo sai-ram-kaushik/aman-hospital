8b85d561e64641f7de09c0b240f6ccd26369b27a
Sairam Kaushik <sairamkaushik1@gmail.com>
2023-03-11 13:26:23 +0530

    intial commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='fixed bg-white shadow-xl w-full h-20'>
+        <h1>hello</h1>
+    </div>
+  )
+}
+
+export default Navbar
